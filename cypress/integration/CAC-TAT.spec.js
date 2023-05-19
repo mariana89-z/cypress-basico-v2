8f719296986da06919de20e2f2be0b764ce2fd2f
mariana89-z <mariana-cristhina@hotmail.com>
2023-05-18 22:21:34 -0300

    Primeiro commit

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+	beforeEach(function() {
+		cy.visit('./src/index.html')
+	})
+
+	it('verifica o título da aplicação', function() {
+		cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+	it.only('preenche os campos obrigatórios e envia o formulário', function() {
+		cy.get('#firstName').type('Mariana')
+		cy.get('#lastName').type('Zeferino')
+		cy.get('#email').type('mariana.cristhina89@gmail.com')
+		cy.get('#open-text-area').type('Teste')
+		cy.get('.button[type="submit"]').click()
+
+		cy.get('.success').should('be.visible')
+		cy.get('.error').should('Exibe mensagem de erro ao submenter o formulário com um email com formatação inválida')
+
+    })
+  })
