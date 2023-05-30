93e775c6c28f8883266d8aef0570a8cc0b962fb0
mariana89-z <mariana-cristhina@hotmail.com>
2023-05-29 22:27:22 -0300

    Exibição mensagem de erro

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,14 +10,27 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
 	it('preenche os campos obrigatórios e envia o formulário', function() {
+      const longText = 'Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste.' 
+
 		cy.get('#firstName').type('Mariana')
 		cy.get('#lastName').type('Zeferino')
 		cy.get('#email').type('mariana.cristhina89@gmail.com')
 		cy.get('#open-text-area').type('Teste')
+		cy.get('#open-text-area').type(longText, { delay: 0})
 		cy.get('.button[type="submit"]').click()
 
 		cy.get('.success').should('be.visible')
-		cy.get('.error').should('Exibe mensagem de erro ao submenter o formulário com um email com formatação inválida')
+	})
 
-    })
+	it('Exibe mensagem de erro ao submenter o formulário com um email com formatação inválida', function() {
+		cy.get('#firstName').type('Mariana')
+		cy.get('#lastName').type('Zeferino')
+		cy.get('#email').type('mariana.cristhina89@gmail,com')
+		cy.get('#open-text-area').type('Teste')
+		cy.get('.button[type="submit"]').click()
+
+		cy.get('.error').should('be.visible')
+	})
+
+
   })
